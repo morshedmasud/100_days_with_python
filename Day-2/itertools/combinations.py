f43e381ae9a6ab69c49ed48fd3310fd6c00ffd49
Masud Morshed <masudraj6@gmail.com>
2018-04-20 17:35:00 +0600

    combination function added

---
+++ Day-2/itertools/combinations.py
@@ -0,0 +1,8 @@
+# Go to the link to know about the problem
+# https://www.hackerrank.com/challenges/itertools-combinations/problem
+from itertools import combinations
+
+s, k = input().split()
+for i in range(1, int(k) + 1):
+    for j in combinations(sorted(s), i):
+        print("".join(j))
