c8550e4b2c104051cc175da7bbb8b62e47df0f66
Masud Morshed <masudraj6@gmail.com>
2018-04-21 22:58:00 +0600

    combination problem 2 added

---
+++ Day-3/itertools/combi_2.py
@@ -0,0 +1,14 @@
+# Go to the link to know about the problem
+from itertools import combinations
+
+n = int(input())
+s = input().split()
+k = int(input())
+
+num = 0
+den = 0
+for i in combinations(s, k):
+    den += 1
+    num += 'a' in i
+
+print(float(num/den))
