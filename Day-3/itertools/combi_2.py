7ca7b3bd1ae7539e982baa96be93068575aef33f
Masud Morshed <masudraj6@gmail.com>
2018-04-22 02:35:45 +0600

    updated

--- Day-3/itertools/combi_2.py
+++ Day-3/itertools/combi_2.py
@@ -16,8 +16,8 @@ print(float(num/den))
 
 
 
-from itertools import product
-k, m = map(int, input().split())
-n = (list(map(int, input().split()))[1:] for _ in range(k))
-result = map(lambda x: sum(i**2 for i in x)%m, product(*n))
-print(max(result))
+# from itertools import product
+# k, m = map(int, input().split())
+# n = (list(map(int, input().split()))[1:] for _ in range(k))
+# result = map(lambda x: sum(i**2 for i in x)%m, product(*n))
+# print(max(result))
--- Day-3/itertools/maximize.py
+++ Day-3/itertools/maximize.py
@@ -1,7 +1,6 @@
 # Go to the link to know about the problem
 # https://www.hackerrank.com/challenges/maximize-it/problem
 
-from itertools import product
 k, m = map(int, input().split())
 n = (list(map(int, input().split()))[1:] for _ in range(k))
-print(n)
+
