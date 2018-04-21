013fb1ee7822fc9eaa5f210686fb5385ae38a3cc
Masud Morshed <masudraj6@gmail.com>
2018-04-22 04:41:15 +0600

    maximize added

--- Day-3/itertools/maximize.py
+++ Day-3/itertools/maximize.py
@@ -1,6 +1,21 @@
 # Go to the link to know about the problem
 # https://www.hackerrank.com/challenges/maximize-it/problem
 
-k, m = map(int, input().split())
-n = (list(map(int, input().split()))[1:] for _ in range(k))
+from itertools import product
 
+k, m = map(int, input().split())
+n = []
+for _ in range(k):
+    n.append(list(map(int, input().split()))[1:])
+temp = []
+
+def test(arr):
+    total = 0
+    for i in arr:
+        total += i*i
+    return total%m
+
+for j in product(*n):
+    temp.append(test(j))
+
+print(max(temp))
