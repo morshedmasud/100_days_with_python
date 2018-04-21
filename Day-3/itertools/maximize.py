0b9c8856f884ec8d6164f6376d32ede737e09e89
Masud Morshed <masudraj6@gmail.com>
2018-04-22 02:32:10 +0600

    updated

--- Day-3/itertools/maximize.py
+++ Day-3/itertools/maximize.py
@@ -1,17 +1,7 @@
 # Go to the link to know about the problem
 # https://www.hackerrank.com/challenges/maximize-it/problem
+
 from itertools import product
-
 k, m = map(int, input().split())
-lis = []
-for _ in range(k):
-    n = (list(map(int, input().split()))[1:])
-
+n = (list(map(int, input().split()))[1:] for _ in range(k))
 print(n)
-
-# result = sum(map(lambda x: x**2, lis)) % m
-# print(result)
-
-# print(lis)
-
-# print(list(product(*lis)))
