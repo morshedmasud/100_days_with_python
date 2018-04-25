0cbe09785e03f36237a760e0d00f80ba14ebad00
Masud Morshed <masudraj6@gmail.com>
2018-04-26 00:57:52 +0600

    updated

--- Day-4/temp.py
+++ Day-4/temp.py
@@ -1,2 +1,15 @@
+import re
+p = r"[A-Z](.*)"
+a = []
 with open('file.txt', 'r') as f:
-    linedsf.readline()
+    for i in f:
+        a.append(i.split())
+
+
+
+for i in a:
+    for j in i:
+        if j[0]>"A" and j<"Z":
+            with open("new_list.txt", "a") as f2:
+                f2.write(("%s\n"%j))
+
