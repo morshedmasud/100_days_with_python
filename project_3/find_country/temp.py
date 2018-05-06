7b333c1ac7bf9f6bb0dd20e940d404b472992ed5
Masud Morshed <masudraj6@gmail.com>
2018-05-06 17:01:53 +0600

    updated

--- project_3/find_country/temp.py
+++ project_3/find_country/temp.py
@@ -1,15 +1,25 @@
 import re
-p = r"[A-Z](.*)"
+# p = r"[A-Z](.*)"
+p = r""
 a = []
 with open('file.txt', 'r') as f:
-    for i in f:
-        a.append(i.split())
+    a = f.readline(5)
+    print(a)
+    # temp = re.findall(p, a)
+    # print(temp)
+
+
+#     for i in f:
+#         a.append(i.split())
+# for i in a:
+#     with open("new_list.txt", "a") as n:
 
 
 
-for i in a:
-    for j in i:
-        if j[0]>"A" and j<"Z":
-            with open("new_list.txt", "a") as f2:
-                f2.write(("%s\n"%j))
+
+# for i in a:
+#     for j in i:
+#         if j[0]>"A" and j<"Z":
+#             with open("new_list.txt", "a") as f2:
+#                 f2.write(("%s\n"%j))
 
