22fdc945d5b409e33db664462004a5b1f4543d0b
Masud Morshed <masudraj6@gmail.com>
2018-05-09 17:03:18 +0600

    updated

--- project_3/find_country/temp.py
+++ project_3/find_country/temp.py
@@ -1,13 +1,13 @@
-import re
-p = r"([\d\.-]+)"
-a = []
-with open('file.txt', 'r') as f:
-    for j in f:
-        temp = re.search(r'([\w\.-]+)\B([\D\.-]+)', j)
-        n = temp.group()
-        with open(n[0]+'.text', 'a') as f:
-            f.write(n)
-            f.write("\n")
+# import re
+# p = r"([\d\.-]+)"
+# a = []
+# with open('file.txt', 'r') as f:
+#     for j in f:
+#         temp = re.search(r'([\w\.-]+)\B([\D\.-]+)', j)
+#         n = temp.group()
+#         with open(n[0]+'.text', 'a') as f:
+#             f.write(n)
+#             f.write("\n")
         # with open('new.txt', 'a') as n:
         #     n.write(temp.group())
         #     n.write('\n')
