26d53707b2ae38ab10f64a63ca438f129f008991
Masud Morshed <masudraj6@gmail.com>
2018-05-11 16:20:09 +0600

    updated

--- Day-6/datetime/Calendar_Module.py
+++ Day-6/datetime/Calendar_Module.py
@@ -2,6 +2,7 @@
 # https://www.hackerrank.com/challenges/calendar-module/problem
 
 import calendar
-
+# input from user for month, day, year
 m, d, y = map(int, input().split())
+# first we find out the weekday and then we make the real name for day using calendar.day_name()
 print(calendar.day_name[calendar.weekday(y, m, d)].upper())
