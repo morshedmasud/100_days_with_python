5f2f4b15d263299c3bc2d03830292cfc27f16e0d
Masud Morshed <masudraj6@gmail.com>
2018-05-14 15:46:35 +0600

    AjobTurtle class from turtle

---
+++ Day-7/ajobturlte.py
@@ -0,0 +1,9 @@
+import turtle
+
+class AjobTurtle(turtle.Turtle):
+    """AjobTurtle is a class for new type of turtle"""
+    pass
+
+if __name__ == "__main__":
+    montu = AjobTurtle()
+    print(type(montu))
