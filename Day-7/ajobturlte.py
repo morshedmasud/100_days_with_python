065280606a67f042574646dcebd0ce7dffdb4a9c
Masud Morshed <masudraj6@gmail.com>
2018-05-14 16:09:28 +0600

    create e

--- Day-7/ajobturlte.py
+++ Day-7/ajobturlte.py
@@ -2,8 +2,21 @@ import turtle
 
 class AjobTurtle(turtle.Turtle):
     """AjobTurtle is a class for new type of turtle"""
-    pass
+    def forward(self, pixel):
+        super().backward(pixel)
+
+    def backward(self, pixel):
+        super().forward(pixel)
+
+    def left(self, pixel):
+        super().right(pixel)
+
+    def right(self, pixel):
+        print("I won't turn right, because I am Ajob!!")
 
 if __name__ == "__main__":
     montu = AjobTurtle()
-    print(type(montu))
+    montu.left(30)
+    montu.forward(200)
+    montu.right(50)
+    turtle.done()
