967af8b753b5967d257bebf24d9e82036a68f0de
Masud Morshed <masudraj6@gmail.com>
2018-05-15 11:39:35 +0600

    updated

--- Day-7/regex.py
+++ Day-7/regex.py
@@ -1,8 +1,15 @@
 import re
+""" Find the phone number in a string"""
 # s = "House no: 4, phone no: 016  70397894 ihgoeri giiof 00000000654 fggifdg01821598697 jgkgj 01857457130"
 # result = re.findall(r'01[56789]\s*\d{8}', s)
 # print(result)
+""" Find the full word with space"""
+# s = "Bangladesh, India, South Koria, New Zealand, America,"
+# r = re.findall(r'(\w+\s*\w+)', s)
+# print(r)
 
-s = "Bangladesh, India, South Koria, New Zealand, America,"
-r = re.findall(r'(\w+\s*\w+)', s)
-print(r)
+
+with open('temp.txt', 'r') as f:
+    temp = f.read()
+    print(temp)
+
