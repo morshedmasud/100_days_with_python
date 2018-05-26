60db2e3ecaeb556ce00195ef411d00a4f6f1ea6f
Masud Morshed <masudraj6@gmail.com>
2018-05-27 00:09:10 +0600

    added

--- Day-9/web_crawling.py
+++ Day-9/web_crawling.py
@@ -1,37 +1,37 @@
 import requests
 import re
 
-# # main_url = "http://books.toscrape.com/index.html"
-# # # Get the page content
-# # respose = requests.get(main_url)
-# # page_content = respose.text
+# main_url = "http://books.toscrape.com/index.html"
+# # Get the page content
+# respose = requests.get(main_url)
+# page_content = respose.text
 
-# # # Just only taking books categories from page content
-# # all_cat = re.compile(r'<div class="side_categories">(.*?)</div>', re.M | re.DOTALL)
-# # result = all_cat.findall(page_content)
+# # Just only taking books categories from page content
+# all_cat = re.compile(r'<div class="side_categories">(.*?)</div>', re.M | re.DOTALL)
+# result = all_cat.findall(page_content)
 
-# # # Taking books categories link
-# # text2 = result[0]
-# # catg_pat = re.compile(r'<li>\s*<a href="(.*?)">\s*(\w+[\s\w]+\w)\s*?', re.M | re.DOTALL)
-# # ctg = catg_pat.findall(text2)
+# # Taking books categories link
+# text2 = result[0]
+# catg_pat = re.compile(r'<li>\s*<a href="(.*?)">\s*(\w+[\s\w]+\w)\s*?', re.M | re.DOTALL)
+# ctg = catg_pat.findall(text2)
 
 
-# # # Taking only mystery categories form ctg(all_book_categories)
-# # mystery ="http://books.toscrape.com/"+ctg[2][0]
-# # rsp = requests.get(mystery)
-# # text3 = rsp.text
-# # print(text3)
+# # Taking only mystery categories form ctg(all_book_categories)
+# mystery ="http://books.toscrape.com/"+ctg[2][0]
+# rsp = requests.get(mystery)
+# text3 = rsp.text
+# print(text3)
 
-# # # Taking all the books url from mystery cetegories
-# # text3 = text3.replace("\n", " ")
-# # book_pat = re.compile(r'<h3><a href="(.*?)"\s*title="(.*?)">')
-# # my_bk_link = book_pat.findall(text3)
+# # Taking all the books url from mystery cetegories
+# text3 = text3.replace("\n", " ")
+# book_pat = re.compile(r'<h3><a href="(.*?)"\s*title="(.*?)">')
+# my_bk_link = book_pat.findall(text3)
 
-# #  # Making next page url
-# # next_pat = re.compile(r' <li class="next"><a href="(.*?)">next</a></li>')
-# # next_link = next_pat.findall(text3)
-# # nex = mystery.rfind('/')
-# # nex_l = mystery[:nex+1] + next_link[0]
+#  # Making next page url
+# next_pat = re.compile(r' <li class="next"><a href="(.*?)">next</a></li>')
+# next_link = next_pat.findall(text3)
+# nex = mystery.rfind('/')
+# nex_l = mystery[:nex+1] + next_link[0]
 
 
 
