86caf04f5b82c0f66e6407ff5a1dea3b384fdea5
Masud Morshed <masudraj6@gmail.com>
2018-06-24 14:35:54 +0600

    updated

---
+++ project_7/csv_test/venv/mess_database/log.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def set_custom_log_info(filename):
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %P', filename=filename, level=logging.DEBUG)
+
+
+def report(e:Exception):
+    logging.exception(str(e))
